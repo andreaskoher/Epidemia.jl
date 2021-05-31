c3634e8c8d4e572808b7ff160ca36008a6875fcf
Documenter.jl <documenter@juliadocs.github.io>
2021-05-31 12:24:01 +0000

    build based on 4fd051e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Epidemia","category":"page"},{"location":"#Epidemia","page":"Home","title":"Epidemia","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Epidemia.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Epidemia]","category":"page"}]
+}
