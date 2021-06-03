c2413d603058a1705a2e58b82983a2edb07e3bf7
andreaskoher <ankoh@dtu.dk>
2021-06-04 00:33:45 +0200

    remove prior dists from RWModel and create a sampleable RWModelDistribution with corresponding Bijector

--- src/epirt.jl
+++ src/epirt.jl
@@ -1,81 +1,74 @@
-using Random
+import Random
 using Distributions
-using Dates
-using UnPack
-using Bijectors
+using Dates: DateTime, Day
+using UnPack: @unpack
+import Bijectors
 
 abstract type EpiRt end
-
 ## ===========================================================================
 # Random Walk
 
-struct RandomWalk{Tn, Ts} <: ContinuousMultivariateDistribution
-  n::Tn
-  s::Ts
+struct RandomWalkDistribution{Tn, Ts} <: ContinuousMultivariateDistribution
+    n::Tn
+    s::Ts
 end
 
-Random.rand(rng::AbstractRNG, d::RandomWalk{Tn, Ts}) where {Tn, Ts} = begin
-	@unpack n, s = d
-	step_prior = Normal(zero(Ts), s)
-	x = Vector{Ts}(undef, n)
+Random.rand(rng::Random.AbstractRNG, d::RandomWalkDistribution{Tn, Ts}) where {Tn, Ts} = begin
+    @unpack n, s = d
+    step_prior = Normal(zero(Ts), s)
+    x = Vector{Ts}(undef, n)
 
-	######## random walk
-	x[1] = rand(step_prior)
-  for i in 2:n
-    x[i] = x[i-1] + rand(step_prior)
-  end
-  return x
+    ######## random walk
+    x[1] = rand(step_prior)
+    for i in 2:n
+        x[i] = x[i-1] + rand(step_prior)
+    end
+    return x
 end
 
-Distributions.logpdf(d::RandomWalk, x::AbstractVector{T} where T) =
-  Distributions.logpdf(MvNormal(d.n-1, d.s), diff(x))
-Bijectors.bijector(d::RandomWalk) = Identity{1}()
+Distributions.logpdf(d::RandomWalkDistribution{Tn, Ts}, x::AbstractVector{T}) where {Tn, Ts, T} =
+    logpdf( MvNormal( d.n-1, d.s ), diff(x) )# + logpdf( Normal( zero(Ts), x[1] ) )
+Bijectors.bijector(d::RandomWalkDistribution) = Bijectors.Identity{1}()
 
 ## ===========================================================================
 """
 random walk with variable step interval
 """
-struct RW{Tr0, Tσ, Tno, Tnt, Tns, Tin, Tli, Til} <: EpiRt
-  r0_prior    ::Tr0
-	σ_rw_prior  ::Tσ
-	nobs        ::Tno
-	ntotal      ::Tnt
-	nsteps      ::Tns
-	index       ::Tin
-  link        ::Tli
-	invlink     ::Til
+struct RWModel{Tno, Tnt, Tns, Tin, Tli, Til} <: EpiRt
+    nobs        ::Tno
+    ntotal      ::Tnt
+    nsteps      ::Tns
+    index       ::Tin
+    link        ::Tli
+    invlink     ::Til
 end
 
 function stepindex(n, rwstep)
-  index = Vector{Int64}(undef, n)
-  nsteps = 1
-  for i in eachindex(index)
-    index[i] = nsteps
-    i % rwstep == 0 && ( nsteps += 1 )
-  end
-  index
+    index = Vector{Int64}(undef, n)
+    nsteps = 1
+    for i in eachindex(index)
+        index[i] = nsteps
+        i % rwstep == 0 && ( nsteps += 1 )
+    end
+    index
 end
 
-RW(
-	dates     ::Vector{DateTime};
-	endobs    ::DateTime = dates[end],
-	r0_prior  ::ContinuousUnivariateDistribution = truncated(Normal(3.5, 1.), 0, Inf),
-  σ_rw_prior::ContinuousUnivariateDistribution = truncated(Normal(0.02,0.01), 0, Inf),
-	rwstep    ::Int = 1,
-	link = exp,
-	invlink = log,
+RWModel(
+    dates     ::AbstractVector;
+    endobs    = dates[end],
+    rwstep    ::Int = 1,
+    link      = exp,
+    invlink   = log,
 ) = begin
 
-	@assert all( dates[1]:Day(1):dates[end] .== dates )
+	#@assert all( dates[1]:Day(1):dates[end] .== dates )
 	ntotal  = length(dates)
 	nobs    = findfirst(==(endobs), dates)
-  index   = stepindex(nobs-1, rwstep)
-  nsteps  = length( unique( index ) )
+    index   = stepindex(nobs-1, rwstep)
+    nsteps  = length( unique( index ) )
 
-	RW(
-		r0_prior,
-		σ_rw_prior,
-		nobs,
+	RWModel(
+        nobs,
 		ntotal,
 		nsteps,
 		index,
@@ -84,97 +77,126 @@ RW(
 	)
 end
 
-(epirt::RW{Tr0, Tσ, Tno, Tnt, Tns, Tin, Tli, Til})(
+(epirt::RWModel{Tno, Tnt, Tns, Tin, Tli, Til})(
 	θ, ::Val{false}
-) where {Tr0, Tσ, Tno, Tnt, Tns, Tin, Tli, Til} = begin
-  @unpack rw, r0 = θ
-  @unpack nobs, link = epirt
+) where {Tno, Tnt, Tns, Tin, Tli, Til} = begin
+    @unpack rw, r0 = θ
+    @unpack nobs, link = epirt
 
-  rt         = Vector{typeof(r0)}(undef, nobs)
-  rt[1]      = r0
-	rt[2:nobs] = link.(rw)
-  return rt
+    rt         = Vector{typeof(r0)}(undef, nobs)
+    rt[1]      = r0
+    rt[2:nobs] = link.(rw)
+    return rt
 end
 
-(epirt::RW{Tr0, Tσ, Tno, Tnt, Tns, Tin, Tli, Til})(
+(epirt::RWModel{Tno, Tnt, Tns, Tin, Tli, Til})(
 	θ, ::Val{true}
-) where {Tr0, Tσ, Tno, Tnt, Tns, Tin, Tli, Til} = begin
-  @unpack rw, r0 = θ
-  @unpack ntotal, nobs, link = epirt
+) where {Tno, Tnt, Tns, Tin, Tli, Til} = begin
+    @unpack rw, r0 = θ
+    @unpack ntotal, nobs, link = epirt
 
-  rt              = Vector{typeof(r0)}(undef, ntotal)
-  rt[1]           = r0
-	rt[2:nobs]      = link.(rw)
-  rt[nobs+1:end] .= rt[nobs]
-  return rt
+    rt              = Vector{typeof(r0)}(undef, ntotal)
+    rt[1]           = r0
+    rt[2:nobs]      = link.(rw)
+    rt[nobs+1:end] .= rt[nobs]
+    return rt
 end
 
-Base.rand(rng::AbstractRNG, epirt::RW) = begin
-	@unpack r0_prior, σ_rw_prior, nsteps, invlink = epirt
+## =============================================================================
 
-	######## draw from univariate prior
-	σ_rw = rand(rng, σ_rw_prior )
-	r0   = rand(rng, r0_prior )
-
-	######## draw from random walk prior
-	rw0 = invlink(r0)
-	rw_prior = RandomWalk(nsteps, σ_rw)
-	rw = rand(rng, rw_prior)
-  return (; σ_rw, r0, rw)
+struct RWModelDistribution2{Tr0, Tσ, Tbm} <: ContinuousMultivariateDistribution
+	r0_prior   ::Tr0
+	σ_rw_prior ::Tσ
+	rwmodel::Tbm
 end
 
-Distributions.logpdf(epirt::RW, θ) = begin
-  @unpack rw, r0, σ_rw = θ
-	@unpack r0_prior, σ_rw_prior, nsteps, invlink = epirt
+# RWModelDistribution2(
+# 	r0_prior   ::Distribution,
+# 	σ_rw_prior ::Distribution,
+# 	args...; kwargs...
+# ) = begin
+# 	RWModelDistribution2(
+# 		r0_prior, σ_rw_prior,
+# 		RWModel(args...; kwargs...)
+# 	)
+# end
 
-	rw_prior   = RandomWalk(nsteps, σ_rw)
-	step_prior = Normal(zero(σ_rw), σ_rw) #NOTE same prior as in RadomWalk
-	step = rw[1] - log(r0)
+Base.rand(rng::Random.AbstractRNG, epirt::RWModelDistribution2) = begin
+	@unpack r0_prior, σ_rw_prior, rwmodel = epirt
+    @unpack nsteps, invlink = rwmodel
 
-	target  = logpdf(σ_rw_prior, σ_rw)
-  target += logpdf(r0_prior, r0)
-	target += logpdf(step_prior, step)
-	target += logpdf(rw_prior, rw)
-  return target
+    ######## draw from univariate prior
+    σ_rw = rand(rng, σ_rw_prior )
+    r0   = rand(rng, r0_prior )
+
+    ######## draw from random walk prior
+    rw_prior = RandomWalkDistribution(nsteps, σ_rw)
+    rw = rand(rng, rw_prior)
+    return (; σ_rw, r0, rw)
 end
+
+Distributions.logpdf(epirt::RWModelDistribution2, θ) = begin
+    @unpack rw, r0, σ_rw = θ
+    @unpack r0_prior, σ_rw_prior, rwmodel = epirt
+	@unpack nsteps, invlink = rwmodel
+
+    rw_prior   = RandomWalkDistribution(nsteps, σ_rw)
+    step_prior = Normal(zero(σ_rw), σ_rw) #NOTE same prior as in RandomWalkDistribution
+    step = first(rw) - invlink(r0)
+
+    target  = logpdf(σ_rw_prior, σ_rw)
+    target += logpdf(r0_prior, r0)
+    target += logpdf(step_prior, step)
+    target += logpdf(rw_prior, rw)
+    return target
+end
+
+Bijectors.bijector(d::RWModelDistribution2) = begin
+	@unpack r0_prior, σ_rw_prior = d
+	bj_r0 = Bijectors.bijector(r0_prior)
+	bj_σ  = Bijectors.bijector(σ_rw_prior)
+	bj_rw = Bijectors.Identity{1}()
+	return Bijectors.Stacked( (bj_r0, bj_σ, bj_rw) )
+end
+
 
 ## ===========================================================================
 """
 random walk with constant early phase
 """
-struct ConstantRW{Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til} <: EpiRt
-  r0_prior    ::Tr0
-  r1_prior    ::Tr1
-	σ_rw_prior  ::Tσ
-	nconst      ::Tnc
-	nobs        ::Tno
-	ntotal      ::Tnt
-	nsteps      ::Tns
-	index       ::Tin
-  link        ::Tli
-	invlink     ::Til
+struct ConstantPhaseRWModel{Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til} <: EpiRt
+    r0_prior    ::Tr0
+    r1_prior    ::Tr1
+    σ_rw_prior  ::Tσ
+    nconst      ::Tnc
+    nobs        ::Tno
+    ntotal      ::Tnt
+    nsteps      ::Tns
+    index       ::Tin
+    link        ::Tli
+    invlink     ::Til
 end
 
-ConstantRW(
-	dates     ::Vector{DateTime},
-	startrw   ::DateTime;
-	endobs    ::DateTime = dates[end],
-	r0_prior  ::ContinuousUnivariateDistribution = truncated(Normal(3.5, 1.), 0, Inf),
-	r1_prior  ::ContinuousUnivariateDistribution = truncated(Normal(1.,0.5), 0, Inf),
-  σ_rw_prior::ContinuousUnivariateDistribution = truncated(Normal(0.02,0.01), 0, Inf),
-	rwstep    ::Int = 1,
-	link = exp,
-	invlink = log,
+ConstantPhaseRWModel(
+    dates     ::Vector{DateTime},
+    startrw   ::DateTime;
+    endobs    ::DateTime = dates[end],
+    r0_prior  ::ContinuousUnivariateDistribution = truncated(Normal(3.5, 1.), 0, Inf),
+    r1_prior  ::ContinuousUnivariateDistribution = truncated(Normal(1.,0.5), 0, Inf),
+    σ_rw_prior::ContinuousUnivariateDistribution = truncated(Normal(0.02,0.01), 0, Inf),
+    rwstep    ::Int = 1,
+    link = exp,
+    invlink = log,
 ) = begin
 
-	@assert all( dates[1]:Day(1):dates[end] .== dates )
-	ntotal  = length(dates)
-	nconst  = findfirst(==(startrw), dates)-1
-	nobs    = findfirst(==(endobs), dates)
-  index   = stepindex(nobs-nconst-1, rwstep)
-  nsteps  = length( unique( index ) )
+    @assert all( dates[1]:Day(1):dates[end] .== dates )
+    ntotal  = length(dates)
+    nconst  = findfirst(==(startrw), dates)-1
+    nobs    = findfirst(==(endobs), dates)
+    index   = stepindex(nobs-nconst-1, rwstep)
+    nsteps  = length( unique( index ) )
 
-	ConstantRW(
+	ConstantPhaseRWModel(
 		r0_prior,
 		r1_prior,
 		σ_rw_prior,
@@ -188,69 +210,64 @@ ConstantRW(
 	)
 end
 
-(epirt::ConstantRW{Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til})(
+(epirt::ConstantPhaseRWModel{Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til})(
 	θ, ::Val{false}
 ) where {Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til} = begin
-  @unpack rw, r0, r1 = θ
-  @unpack nconst, nobs, link = epirt
+    @unpack rw, r0, r1 = θ
+    @unpack nconst, nobs, link = epirt
 
-  rt = Vector{typeof(r0)}(undef, nobs)
-  rt[1:nconst]     .= r0
-	rt[nconst+1]      = r1
-	rt[nconst+2:end]  = link.(rw)
-  return rt
+    rt = Vector{typeof(r0)}(undef, nobs)
+    rt[1:nconst]     .= r0
+    rt[nconst+1]      = r1
+    rt[nconst+2:end]  = link.(rw)
+    return rt
 end
 
-(epirt::ConstantRW{Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til})(
+(epirt::ConstantPhaseRWModel{Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til})(
 	θ, ::Val{true}
 ) where {Tr0, Tr1, Tσ, Tnc, Tno, Tnt, Tns, Tin, Tli, Til} = begin
-  @unpack rw, r0, r1 = θ
-  @unpack nconst, ntotal, nobs, link, index = epirt
+    @unpack rw, r0, r1 = θ
+    @unpack nconst, ntotal, nobs, link, index = epirt
 
-  rt = Vector{typeof(r0)}(undef, ntotal)
-	rt[1:nconst]      .= r0
-	rt[nconst+1]       = r1
-	rt[nconst+2:nobs]   = link.(rw[index])
-	rt[nobs+1:end]    .= rt[nobs]
-  return rt
+    rt = Vector{typeof(r0)}(undef, ntotal)
+    rt[1:nconst]      .= r0
+    rt[nconst+1]       = r1
+    rt[nconst+2:nobs]  = link.(rw[index])
+    rt[nobs+1:end]    .= rt[nobs]
+    return rt
 end
 
-Base.rand(rng::AbstractRNG, epirt::ConstantRW) = begin
-	@unpack r0_prior, r1_prior, σ_rw_prior, nsteps, invlink = epirt
+Base.rand(rng::Random.AbstractRNG, epirt::ConstantPhaseRWModel) = begin
+    @unpack r0_prior, r1_prior, σ_rw_prior, nsteps, invlink = epirt
 
-	######## draw from univariate prior
-	σ_rw = rand(rng, σ_rw_prior )
-	r0   = rand(rng, r0_prior )
-	r1   = rand(rng, r1_prior )
+    ######## draw from univariate prior
+    σ_rw = rand(rng, σ_rw_prior )
+    r0   = rand(rng, r0_prior )
+    r1   = rand(rng, r1_prior )
 
-	######## draw from random walk prior
-	rw0 = invlink(r1)
-	rw_prior = RandomWalk(nsteps, σ_rw)
-	rw = rand(rng, rw_prior )
-  return (; σ_rw, r0, r1, rw)
+    ######## draw from random walk prior
+    rw0 = invlink(r1)
+    rw_prior = RandomWalkDistribution(nsteps, σ_rw)
+    rw = rand(rng, rw_prior )
+    return (; σ_rw, r0, r1, rw)
 end
 
-Distributions.logpdf(epirt::ConstantRW, θ) = begin
-  @unpack rw, r0, r1, σ_rw = θ
-	@unpack r0_prior, r1_prior, σ_rw_prior, nsteps, invlink = epirt
+Distributions.logpdf(epirt::ConstantPhaseRWModel, θ) = begin
+    @unpack rw, r0, r1, σ_rw = θ
+    @unpack r0_prior, r1_prior, σ_rw_prior, nsteps, invlink = epirt
 
-	rw_prior   = RandomWalk(nsteps, σ_rw)
-	step_prior = Normal(zero(σ_rw), σ_rw) #NOTE same prior as in RadomWalk
-	step = rw[1] - invlink(r1)
+    rw_prior   = RandomWalkDistribution(nsteps, σ_rw)
+    step_prior = Normal(zero(σ_rw), σ_rw) #NOTE same prior as in RadomWalk
+    step = rw[1] - invlink(r1)
 
-	target  = logpdf(σ_rw_prior, σ_rw)
-  target += logpdf(r0_prior, r0)
-  target += logpdf(r1_prior, r1)
-	target += logpdf(step_prior, step)
-	target += logpdf(rw_prior, rw)
-  return target
+    target  = logpdf(σ_rw_prior, σ_rw)
+    target += logpdf(r0_prior, r0)
+    target += logpdf(r1_prior, r1)
+    target += logpdf(step_prior, step)
+    target += logpdf(rw_prior, rw)
+    return target
 end
 
-# dates = DateTime("2020-04-01"):Day(1):DateTime("2021-01-01") |> collect
-# epirt = ConstantRW(dates, DateTime("2020-04-02"))
-# θ = rand(epirt)
-# rt = epirt(θ, Val(false))
-# logpdf(epirt, θ)
 ## ===========================================================================
 
 
@@ -294,7 +311,7 @@ end
 #
 # prior_scale: The steps of the walks are independent zero mean normal with an unknown scale hyperparameter. This scale is given a half-normal prior. prior_scale sets the scale parameter of this hyperprior.
 # """
-# rw(time = "date", gr = "country", prior_scale = 0.2) = RW(time, gr, prior_scale)
+# rw(time = "date", gr = "country", prior_scale = 0.2) = RWModel(time, gr, prior_scale)
 
 # epirt(
 #   formula,
